5e76f37349e8a5fd413537250e3800c507533e56
Rachana-W <85270318+Rachana-W@users.noreply.github.com>
2021-06-03 12:18:42 +0530

    Delete Areas/HelpPage directory

--- Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace APIAss.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
